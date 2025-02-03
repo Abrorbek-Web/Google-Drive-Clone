cb08cf95c7c651556fbc5703d22ed1273f2d8f43
Abrorbek <abrorbekweb@gmail.com>
2025-02-03 17:04:07 +0500

    changed

--- app/(root)/cloud/page.tsx
+++ app/(root)/cloud/page.tsx
@@ -1,70 +1,66 @@
-import Header from "@/components/shared/header";
-import ListItem from "@/components/shared/list-item";
-import Storage from "@/components/shared/storage";
+import Header from '@/components/shared/header'
+import ListItem from '@/components/shared/list-item'
+import Storage from '@/components/shared/storage'
 import {
-  Table,
-  TableBody,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { db } from "@/lib/firebase";
-import { currentUser } from "@clerk/nextjs/server";
-import { collection, getDocs, query, where } from "firebase/firestore";
-import React from "react";
+	Table,
+	TableBody,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table'
+import { db } from '@/lib/firebase'
+import { currentUser } from '@clerk/nextjs/server'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import React from 'react'
 
 const getData = async (uid: string) => {
-  try {
-    if (!uid) return [];
-    let data: any[] = [];
-    const q = query(
-      collection(db, "files"),
-      where("uid", "==", uid),
-      where("isArchive", "==", false)
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      data.push({ ...doc.data(), id: doc.id });
-    });
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
+	let data: any[] = []
+	const q = query(
+		collection(db, 'files'),
+		where('uid', '==', uid),
+		where('isArchive', '==', false)
+	)
+	const querySnapshot = await getDocs(q)
+	querySnapshot.forEach(doc => {
+		data.push({ ...doc.data(), id: doc.id })
+	})
+
+	return data
+}
 
 const ClouPage = async () => {
-  const user = await currentUser();
+	const user = await currentUser()
 
-  const files = await getData(user?.id!);
+	const files = await getData(user?.id!)
 
-  const totalSize = files.reduce((acc, file) => acc + file.size, 0);
+	const totalSize = files.reduce((acc, file) => acc + file.size, 0)
 
-  return (
-    <>
-      <Header label="Storage" />
-      <Storage totalSize={JSON.parse(JSON.stringify(totalSize))} />
+	return (
+		<>
+			<Header label='Storage' />
+			<Storage totalSize={JSON.parse(JSON.stringify(totalSize))} />
 
-      <Table className="mt-4">
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Owner</TableHead>
-            <TableHead>Created at</TableHead>
-            <TableHead>File size</TableHead>
-            <TableHead className="text-right">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {files.map((folder) => (
-            <ListItem
-              key={folder.id}
-              item={JSON.parse(JSON.stringify(folder))}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </>
-  );
-};
+			<Table className='mt-4'>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Name</TableHead>
+						<TableHead>Owner</TableHead>
+						<TableHead>Created at</TableHead>
+						<TableHead>File size</TableHead>
+						<TableHead className='text-right'>Actions</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{files.map(folder => (
+						<ListItem
+							key={folder.id}
+							item={JSON.parse(JSON.stringify(folder))}
+						/>
+					))}
+				</TableBody>
+			</Table>
+		</>
+	)
+}
 
-export default ClouPage;
+export default ClouPage
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,19 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "sammi-a2d49.firebaseapp.com",
-  projectId: "sammi-a2d49",
-  storageBucket: "sammi-a2d49.firebasestorage.app",
-  messagingSenderId: "127113662672",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: 'sammi-a2d49.firebaseapp.com',
+	projectId: 'sammi-a2d49',
+	storageBucket: 'sammi-a2d49.firebasestorage.app',
+	messagingSenderId: '127113662672',
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+}
 
-!getApps().length ? initializeApp(firebaseConfig) : getApp();
+!getApps().length ? initializeApp(firebaseConfig) : getApp()
 
-const db = getFirestore();
-const storage = getStorage();
+const db = getFirestore()
+const storage = getStorage()
 
-export { db, storage };
+export { db, storage }
